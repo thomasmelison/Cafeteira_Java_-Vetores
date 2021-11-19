f69749752f3d3daf59b7cfafc7e59610cf5e6ad6
Thomas Mello <thomasmelison@hotmail.com>
2021-11-19 17:48:06 -0300

    Código Final com javadoc

--- Cafeteira.java
+++ Cafeteira.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
-/** O CERTOf
+/** 
  * 
- * Cafeteira
- * A classe cafeteira é uma classe que simula uma cafeteira que utiliza capsulas;
- * O atributo liga_desliga diz se a cafeteira está ligada ou não
- * O atributo nivel_agua diz o nivel de água no reservatório da cafeteira
- * O atributo nivel café é o quanto de água o café vai usar (são determinado pelo usuário)
- * O atributo nivel_agua_max guarda a constante do nivel maximo de agua que a cafeteira suporta
+ * Cafeteira em Java
+ * 
+ * author Thomas Mello thomasmelison@hotmail.com
+ * 
+ * A classe cafeteira é uma classe que simula uma cafeteira 
+ * O atributo botao_on_off diz se a cafeteira está ligada ou não
+ * O atributo nivel_agua diz quanta água tem no reservatório
+ * o atributo sabores tem uma lista de sabores possíveis para o café
+ * O atributo nivel_café tem uma lista de tamanhos de porções de café
+ * O atributo sabor_escolhido tem a string com o sabor escolhido
+ * O atributo quantidade_agua_cafe é o nível escolhido para o tamanho do café
  */
 public class Cafeteira {
 
@@ -17,26 +22,35 @@ public class Cafeteira {
     String sabor_escolhido;
     int quantidade_agua_cafe;
 
-    //provisorio
-    public int getnivelagua(){
-        return nivel_agua;
-    }
 
-    //CRIA A CAFETEIRA--------------------------------------------------
+    /**
+     * Construtor que cria a cafeteira e seta algumas informações iniciais,
+     *  como o nivel da água no reservatório.
+     */
     public Cafeteira(){
         botao_on_off = true;
         nivel_agua = 0;
     }
-    // LIGA E DESLIGA A CAFETEIRA---------------------------------------
+    /**
+     * Metodo que liga ou desliga a cafeteira.
+     */
     public void on_off(){
         botao_on_off = !botao_on_off;
     }
 
+    /**
+     * Metodo que diz se a cafeteira está ligada ou desligada
+     * @return valor booleano se a cafeteira está ligada (true) ou desligada (false)
+     */
     public boolean get_on_off(){
         return botao_on_off;
     }
 
-    // ADICIONA AGUA ---------------------------------------------------
+    /**
+     * Metodo que adiciona água a cafeteira
+     * @param agua_adicionada valor em ml da agua que será adicionada ao reservatorio
+     * @return retorna o valor da quantidade de agua após a ação
+     */
     public int adiciona_Agua(int agua_adicionada){
         if(nivel_agua + agua_adicionada > 500){
             System.out.println("Repositório de Agua Cheio!");
@@ -47,28 +61,52 @@ public class Cafeteira {
         return nivel_agua;
     }
 
-
-
-    //MOVIMENTACAO DE NIVEIS DO CAFE ------------------------------------------
+    /**
+     * Metodo que retorna os valores das porções dos níveis do café
+     * @return  retorna o vetor inteiro com todas as porções possíveis
+     */
     public int[] getNiveisCafe(){
         return nivel_cafe;
     }
 
+    /**
+     * Seleciona uma porção para o café que será feito
+     * @param nivel_cafe tem a informação do índice que corresponde a opção da porção
+     * escolhida na lista nivel_cafe .
+     */
     public void seleciona_nivel_cafe(int nivel_cafe){
         quantidade_agua_cafe = this.nivel_cafe[nivel_cafe];
     }
+
+    /**
+     * Retorna o valor da porção que foi escolhida
+     * @return valor da porção até entao escolhida
+     */
     public int getquantidadeAguaCafe(){
         return quantidade_agua_cafe;
     }
 
-    // movimentacao dos sabores --------------------------------------------------------
+    /**
+     * Retorna a lista de todos possíveis sabores a serem escolhidos
+     * @return lista com a string de todos os sabores
+     */
     public String[] getSabores(){
         return sabores;
     }
+    
+    /**
+     * Retorna o sabor até entao escolhido
+     * @return string com o sabor escolhido
+     */
     public String getSaborEscolhido(){
         return sabor_escolhido;
     }
-    public boolean seleciona_sabor(String sabor){
+
+    /**
+     * Seleciona o sabor escolhido com base na lista de sabores possíveis
+     * @param sabor string com o nome do sabor
+     */
+    public void seleciona_sabor(String sabor){
         boolean sabor_valido = false;
         for(int i = 0 ; i < sabores.length; i++){
             if(sabor.equals( sabores[i] ) ){
@@ -78,13 +116,16 @@ public class Cafeteira {
         }
         if(sabor_valido == false){
             System.out.println("Sabor Inválido");
-            return false;
+            return ;
         }
         sabor_escolhido = sabor;
-        return true;
+        return ;
     }
     
-    //Faz o cafe
+    /**
+     * Metodo que faz o café.
+     * @return retorna uma string dizendo se foi possível fazer o café.
+     */
     public String fazer_café(){
         if(nivel_agua - quantidade_agua_cafe < 0){
             System.out.println("Agua insuficiente para fazer o café!");
@@ -93,8 +134,6 @@ public class Cafeteira {
         nivel_agua = nivel_agua - quantidade_agua_cafe;
         return "Café " + sabor_escolhido + " de " + quantidade_agua_cafe + "ml pronto!";
     }
-
-    
    
 
 }
--- Demonstracao_Cafeteira.java
+++ Demonstracao_Cafeteira.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class Demonstracao_Cafeteira {
 
-    // problema 1 - pode nao digitar um int
-    // problema 2 index out of bounds3
-
+    /**
+     * Função main que demonstra a classe cafeteira
+     * 
+     */
     public static void main(String[] args) {
         
-
+        //Cria a cafeteira e o scanner
         System.out.println("Cafeteira em Java! ");
         Cafeteira cafeteira = new Cafeteira();
         Scanner teclado = new Scanner(System.in);
+
+        // Loop de funcionamento do programa com o menu de opções
         while(true){
             
             System.out.println("Menu:");
@@ -20,9 +23,11 @@ public class Demonstracao_Cafeteira {
             System.out.println("5 - Fazer Café");
             System.out.println("6 - Sair");
             
+            // Checa se o usuário digitou um valor inteiro e o guarda no inteiro opcao.
             is_it_Int(teclado);
             int opcao = teclado.nextInt();
 
+            //Opção 1 Liga ou desliga a cafeiteira dependendo de seu estado inicial
             if(opcao == 1){
                 if(!cafeteira.get_on_off()){
                     System.out.println("Ligando Cafeteira...\n");
@@ -32,18 +37,23 @@ public class Demonstracao_Cafeteira {
                 }
                 cafeteira.on_off();
             }
+
+            //Opção 2 Adiciona água ao reservatório da cafeteira
             else if(opcao == 2){
                 System.out.print("Digite o quanto de agua deseja adicionar (em ml): ");
                 is_it_Int(teclado);
                 int agua_adicionada = teclado.nextInt();
                 System.out.printf("O repositório tem %dml\n",cafeteira.adiciona_Agua(agua_adicionada));
             }
+
+            // Opção 3 Seleciona o sabor do café a ser feito
             else if(opcao == 3){
+                // Checa se a cafeteira está desligada
                 if(!cafeteira.get_on_off()){
                     System.out.println("Ligue a cafeteira primeiro!");
                     continue;
                 }
-
+                // Apresenta ao usuario os sabores da cafeteira
                 String[] sabores = cafeteira.getSabores();
                 System.out.println("Sabores:");
                 for(int i = 0; i < sabores.length; i++){
@@ -59,21 +69,28 @@ public class Demonstracao_Cafeteira {
                     is_it_Int(teclado);
                     index_sabor = teclado.nextInt();
                 }
-                
+                //Usa a função seleciona_sabor para guardar a informação do sabor na classe
                 String sabor = sabores[index_sabor - 1];
                 cafeteira.seleciona_sabor(sabor);
                 System.out.printf("Sabor selecionado: %s\n",sabor);
             }
+
+            // Opção 4 Seleciona o tamanho da porção do café a ser feito
             else if(opcao == 4){
+                //Checa se a cafeteira está ligada
                 if(!cafeteira.get_on_off()){
                     System.out.println("Ligue a cafeteira primeiro!");
                     continue;
                 }
+
+                //Pega os niveis de café disponíveis e os apresenta na tela
                 int[] niveis_cafe = cafeteira.getNiveisCafe();
                 System.out.println("Porções:");
                 for(int i = 0; i < niveis_cafe.length;i++){
                     System.out.printf("%d - %d ml. \n",i + 1,niveis_cafe[i]);
                 }
+
+                // Pede o input do usuario
                 System.out.print("Digite o tamanho da porção:");
                 // Checa se o usuario digitou um inteiro
                 is_it_Int(teclado);
@@ -84,30 +101,38 @@ public class Demonstracao_Cafeteira {
                     is_it_Int(teclado);
                     index_nivel = teclado.nextInt();
                 }
+                // Seleciona o nivel e guarda na classe essa informação
                 cafeteira.seleciona_nivel_cafe(index_nivel - 1);
                 System.out.printf("Tamanho selecionado : %d ml\n",niveis_cafe[index_nivel - 1]);
 
             }
+            // Opção 5 Faz o café
             else if(opcao == 5){
+                // Checa se o usuário ligou a cafeteira
                 if(!cafeteira.get_on_off()){
                     System.out.println("Ligue a cafeteira primeiro!");
                     continue;
                 }
+                // Checa se o usuário escolheu um sabor
                 if(cafeteira.getSaborEscolhido() == null){
                     System.out.println("Selecione um sabor para fazer o cafe!");
                     continue;
                 }
+                // Checa se o usuário escolheu um tamanho do café
                 if(cafeteira.getquantidadeAguaCafe() == 0){
                     System.out.println("Selecione o tamanho do café!");
                     continue;
                 }
-            
+                // Faz o café
                 System.out.println(cafeteira.fazer_café());
             }
+
+            // Opção 6 Fecha o programa
             else if(opcao == 6){
                 System.out.println("Fechando o programa...\n");
                 break;
             }
+            // Qualquer outro inteiro será considerado como opção não encontrada
             else{
                 System.out.println("Opcao nao encontrada...\n");
             }
@@ -115,12 +140,16 @@ public class Demonstracao_Cafeteira {
         }
         
     }
+    /**
+     * Metodo que checa se usuário está digitando valores inteiros e 
+     * fica em loop enquanto o usuário não digitar um valor inteiro.
+     * @param teclado usa o scanner aberto no metodo main para pedir outras entradas do usuario
+     */
     public static void is_it_Int(Scanner teclado){
         while(!teclado.hasNextInt()){
             System.out.print("Digite um inteiro válido: ");
             teclado.next();
         }
     }
-    
     
 }
